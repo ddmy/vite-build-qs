3bfca17d76df2f6569034a4cf902dbcf0e82c3c9
YIjio <841312998@qq.com>
2022-12-20 18:45:19 +0800

    update

---
+++ html/HelloWorld.es.js
@@ -0,0 +1,25 @@
+import { openBlock as c, createElementBlock as _, Fragment as a, createElementVNode as l, toDisplayString as n } from "vue";
+const g = (e, o) => {
+  const t = e.__vccOpts || e;
+  for (const [r, s] of o)
+    t[r] = s;
+  return t;
+}, m = {
+  __name: "HelloWorld",
+  props: {
+    msg: String,
+    msg2: String,
+    msg3: String
+  },
+  setup(e) {
+    return (o, t) => (c(), _(a, null, [
+      l("h1", null, n(e.msg), 1),
+      l("h2", null, n(e.msg2), 1),
+      l("h3", null, n(e.msg3), 1)
+    ], 64));
+  }
+}, u = /* @__PURE__ */ g(m, [["__scopeId", "data-v-74d37222"]]);
+export {
+  u as default
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSGVsbG9Xb3JsZC5lcy5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OzsifQ==
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  msg: String,
+  msg2: String,
+  msg3: String
+})
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>{{ msg2 }}</h2>
+  <h3>{{ msg3 }}</h3>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
